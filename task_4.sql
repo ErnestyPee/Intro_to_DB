3ea2044847605e32be03f678771c08558ddd5daf
Ernest Antwi <ernestypeeforu@gmail.com>
2025-10-19 02:17:26 +0000

    task 4 project

---
+++ task_4.sql
@@ -0,0 +1,13 @@
+-- Display column details for the 'Books' table without using DESCRIBE, EXPLAIN, or ANALYZE
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    COLUMN_KEY,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'Books';
